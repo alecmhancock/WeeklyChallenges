5834ab2807edb96b82882bcf754b8ead680655d4
Alec <alec.m.hancock@gmail.com>
2023-04-07 09:54:43 -0500

    Week 4 complete

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -6,47 +8,114 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            var evenlist = new List<int>();
+            var oddlist = new List<int>();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    evenlist.Add(numbers[i]);
+                }
+                else
+                { 
+                    oddlist.Add(numbers[i]);
+                }
+            }
+            return evenlist.Sum() - oddlist.Sum();
+            
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            var list = new List<string>();
+            list.Add(str1); list.Add(str2); list.Add(str3); list.Add(str4);
+            var answer = list.Min(x => x.Length);
+
+            
+            return answer;
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            var list = new List<int>();
+            list.Add(number1); list.Add(number2); list.Add(number3); list.Add(number4);
+            var answer = list.Min();
+            return answer;
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
-        {
-            throw new NotImplementedException();
+        { 
+          return sideLength1 + sideLength2 > sideLength3 && 
+                sideLength1 + sideLength3 > sideLength2 && 
+                sideLength2 + sideLength3 > sideLength1;
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+           var numCheck = double.TryParse(input, out double number);
+            return numCheck;
+
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            var elementCheck = objs.Count(x => x == null);
+            return elementCheck > objs.Length / 2;
         }
 
         public double AverageEvens(int[] numbers)
-        {
-            throw new NotImplementedException();
+        {   
+            double sum = 0;
+            int count = 0;
+            if (numbers == null || numbers.Length == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                foreach (var number in numbers)
+                {
+                    if (number % 2 == 0)
+                    {
+                        sum += number;
+                        count++;
+                    }
+                }
+                if (count == 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return sum / count;
+                }
+            }
         }
 
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+            var factCheck = 1;
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else if (number == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                for (int i = 1; i <= number; i++)
+                {
+                    factCheck *= i;
+                }
+                return factCheck;
+            }
         }
     }
 }
