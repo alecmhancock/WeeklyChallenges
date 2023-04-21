40a3a6799c5ff7e6386a89bcbedd8c392c20827b
Alec <alec.m.hancock@gmail.com>
2023-04-21 09:51:20 -0500

    Week 6 done, 100% complete with all tests passing.

--- ChallengesWithTestsMark8/ChallengesSet06.cs
+++ ChallengesWithTestsMark8/ChallengesSet06.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,27 +8,115 @@ namespace ChallengesWithTestsMark8
     {
         public bool CollectionContainsWord(IEnumerable<string> words, string word, bool ignoreCase)
         {
-            throw new NotImplementedException();
+            if (words == null || word == null || words.Contains(null))
+            {
+                return false;
+            }
+            foreach (var item in words)
+            {
+                if (ignoreCase == true)
+                {
+                    if (item.ToLower() == word.ToLower())
+                    {
+                        return true;
+                    }
+                    
+                }
+                else
+                {
+                    if (item == word)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
+        
+            
+
+        
 
         public bool IsPrimeNumber(int num)
         {
-            throw new NotImplementedException();
+            if (num < 2)
+            {
+                return false;
+            }
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
+       
 
         public int IndexOfLastUniqueLetter(string str)
         {
-            throw new NotImplementedException();
+            if (str == null)
+            {
+                return -1;
+            }
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                int count = 0;
+                for (int j = str.Length - 1; j >= 0; j--)
+                {
+                    if (str[i] == str[j])
+                    {
+                        count++;
+                    }
+                }
+                if (count == 1)
+                {
+                    return i;
+                }
+            }
+            return -1;
         }
+      
 
         public int MaxConsecutiveCount(int[] numbers)
         {
-            throw new NotImplementedException();
+            int count = 0;
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                int currentCount = 1;
+                for (var j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] != numbers[j])
+                    {
+                        break; 
+                    }
+                    else
+                    {
+                        currentCount++;
+                    }
+                }
+                if (currentCount > count)
+                {
+                    count = currentCount;
+                }
+            }
+            return count;
         }
 
         public double[] GetEveryNthElement(List<double> elements, int n)
         {
-            throw new NotImplementedException();
+            if (elements == null || n <= 0)
+            {
+                return new double[0];
+            }
+            var result = new List<double>();
+            for (int i = n - 1; i < elements.Count; i += n)
+            {
+                result.Add(elements[i]);
+            }
+            return result.ToArray();
+        }
         }
     }
-}
+
